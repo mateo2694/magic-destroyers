062b33952eca490a5fefdc4d1348f8c7a40a2233
Mateo Sánchez <mateo.sanchez@smiledirectclub.com>
2023-02-17 01:29:04 -0500

    Made mage inherit from spellcaster

--- magic-destroyers/Characters/Spellcasters/Mage.cs
+++ magic-destroyers/Characters/Spellcasters/Mage.cs
@@ -3,36 +3,64 @@ using MagicDestroyers.Equipment.Weapons;
 
 namespace MagicDestroyers.Characters.Spellcasters
 {
-    public class Mage
+    public class Mage : Spellcaster
     {
-        private readonly int? abilityPoints;
-        private readonly int? healthPoints;
-        private readonly int? level;
+        private const string DEFAULT_NAME = "Mage";
 
-        private readonly string? faction;
-        private readonly string? name;
+        private const int DEFAULT_LEVEL = 1;
+        private const int DEFAULT_HEALTH_POINTS = 100;
+        private const int DEFAULT_MANA_POINTS = 20;
 
-        private readonly ClothRobe? bodyArmor;
+        private readonly ClothRobe DEFAULT_BODY_ARMOR = new ClothRobe();
 
-        private readonly Staff? weapon;
+        private readonly Staff DEFAULT_WEAPON = new Staff();
 
-        public Mage()
+        public Mage() : this(DEFAULT_NAME, DEFAULT_LEVEL)
         {
         }
 
-        public void ArcaneWrath()
+        public Mage(string name, int level) : this(name, level, DEFAULT_HEALTH_POINTS)
+        {
+        }
+
+        public Mage(string name, int level, int healthPoints)
+        {
+            base.Name = name;
+            base.Level = level;
+            this.HealthPoints = healthPoints;
+            base.ManaPoints = DEFAULT_MANA_POINTS;
+            base.BodyArmor = DEFAULT_BODY_ARMOR;
+            base.Weapon = DEFAULT_WEAPON;
+        }
+
+        private void ArcaneWrath()
         {
             Console.WriteLine("ArcaneWrath");
         }
 
-        public void Firewall()
+        private void Firewall()
         {
             Console.WriteLine("Firewall");
         }
 
-        public void Meditation()
+        private void Meditation()
         {
             Console.WriteLine("Meditation");
+        }
+
+        public override void Attack()
+        {
+            this.ArcaneWrath();
+        }
+
+        public override void SpecialAttack()
+        {
+            this.Firewall();
+        }
+
+        public override void Defend()
+        {
+            this.Meditation();
         }
     }
 }
