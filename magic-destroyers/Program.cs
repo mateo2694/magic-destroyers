4ac7ffdbadc7de689083579ff74df4deb0c50fd7
Mateo Sánchez <mateo.sanchez@smiledirectclub.com>
2023-02-17 02:00:12 -0500

    Set up characters initialization loop

--- magic-destroyers/Program.cs
+++ magic-destroyers/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace MagicDestroyers
+﻿using MagicDestroyers.Characters;
+using MagicDestroyers.Characters.Melee;
+using MagicDestroyers.Characters.Spellcasters;
+
+namespace MagicDestroyers
 {
     class Program
     {
-        static void Main(String[] args)
+        static void Main()
         {
+            List<Character> characters = new List<Character>()
+            {
+                new Warrior(),
+                new Warrior(),
+                new Warrior(),
+                new Mage(),
+                new Mage(),
+                new Mage(),
+            };
+
+            List<Melee> meleeTeam = new List<Melee>();
+            List<Spellcaster> spellTeam = new List<Spellcaster>();
+
+            foreach (var character in characters)
+            {
+                if (character is Melee)
+                {
+                    meleeTeam.Add((Melee)character);
+                }
+                else if (character is Spellcaster)
+                {
+                    spellTeam.Add((Spellcaster)character);
+                }
+            }
         }
     }
 }
