8a33cb9e34552f3187112b26f79c525260e388dd
Mateo Sánchez <mateo.sanchez@smiledirectclub.com>
2023-02-18 16:44:50 -0500

    Extracted a few consts into their own file

---
+++ magic-destroyers/Defaults.cs
@@ -0,0 +1,98 @@
+﻿namespace MagicDestroyers
+{
+    public static class Defaults
+    {
+        public static class Character
+        {
+            public const int MAX_LEVEL = 10;
+            public const int MAX_HEALTH_POINTS = 100;
+        }
+
+        public static class Melee
+        {
+            public const int MAX_ABILITY_POINTS = 50;
+        }
+
+        public static class Assassin
+        {
+
+        }
+
+        public static class Knight
+        {
+
+        }
+
+        public static class Warrior
+        {
+            public const int ABILITY_POINTS = 30;
+            public const string NAME = "Warrior";
+        }
+
+        public static class Spellcaster
+        {
+            public const int MAX_MANA_POINTS = 80;
+        }
+
+        public static class Druid
+        {
+
+        }
+
+        public static class Mage
+        {
+            public const int MANA_POINTS = 40;
+            public const string NAME = "Mage";
+        }
+
+        public static class Necromancer
+        {
+
+        }
+
+        public static class Armor
+        {
+
+        }
+
+        public static class Chainlink
+        {
+
+        }
+
+        public static class ClothRobe
+        {
+
+        }
+
+        public static class LightLeatherVest
+        {
+
+        }
+
+        public static class Weapon
+        {
+
+        }
+
+        public static class Axe
+        {
+
+        }
+
+        public static class Hammer
+        {
+
+        }
+
+        public static class Staff
+        {
+
+        }
+
+        public static class Sword
+        {
+
+        }
+    }
+}
