736644d2a87ae83dafceaaccc7452b40e7811bb3
Mateo Sánchez <mateo.sanchez@smiledirectclub.com>
2023-02-19 23:22:27 -0500

    Implemented game logic

--- magic-destroyers/Program.cs
+++ magic-destroyers/Program.cs
@@ -8,19 +8,20 @@ namespace MagicDestroyers
     {
         static void Main()
         {
-            bool gameOver = false;
+            int round = 0;
+
+            Random random = new Random();
 
             List<Character> characters = new List<Character>()
             {
-                new Warrior(),
-                new Warrior(),
-                new Warrior(),
-                new Mage(),
-                new Mage(),
-                new Mage(),
+                new Warrior("Warrior1", 1),
+                new Warrior("Warrior2", 1),
+                new Mage("Mage1", 1),
+                new Mage("Mage2", 1)
             };
 
             List<Melee> meleeTeam = new List<Melee>();
+
             List<Spellcaster> spellTeam = new List<Spellcaster>();
 
             foreach (var character in characters)
@@ -35,22 +36,62 @@ namespace MagicDestroyers
                 }
             }
 
-            while (!gameOver)
+            Console.WriteLine($"{Environment.NewLine}GAME ON!");
+
+            while (true)
             {
-                // 1. Take random Melee
+                // If all members of a team died game is over
+                if (meleeTeam.Count == 0)
+                {
+                    Console.WriteLine($"{Environment.NewLine}SPELLCASTER TEAM WINS!");
+                    break;
+                }
+                else if (spellTeam.Count == 0)
+                {
+                    Console.WriteLine($"{Environment.NewLine}MELEE TEAM WINS!");
+                    break;
+                }
 
-                // 2. Take random Spellcaster
+                // Take random Melee
+                var meleeCharacter = meleeTeam[random.Next(meleeTeam.Count)];
 
-                // 3. Melee attacks Spellcaster
-                // 3.1 If character died remove it from team
-                // 3.2 If dead get a new character from team
+                // Take random Spellcaster
+                var spellCharacter = spellTeam[random.Next(spellTeam.Count)];
 
-                // 4. Spellcaster attacks Melee
-                // 4.1 If character died remove it from team
-                // 4.2 If dead get a new character from team
+                Console.WriteLine($"{Environment.NewLine}ROUND {++round} -> {meleeCharacter.Name} vs {spellCharacter.Name}");
+                Console.WriteLine();
 
-                // 5. If all members of a team die gameOver = true
+                // Melee attacks Spellcaster
+                Fight(meleeCharacter, spellCharacter);
+
+                // If Spellcaster died remove it from team
+                if (!spellCharacter.IsAlive)
+                {
+                    spellTeam.Remove(spellCharacter);
+                    continue;
+                }
+
+                // Spellcaster attacks Melee
+                Fight(spellCharacter, meleeCharacter);
+
+                // If Melee died remove it from team
+                if (!meleeCharacter.IsAlive)
+                {
+                    meleeTeam.Remove(meleeCharacter);
+                    continue;
+                }
             }
+
+            Console.WriteLine($"{Environment.NewLine}GAME OVER");
+        }
+
+        static void Fight(Character attacker, Character defender)
+        {
+            var isSpecialAttack = new Random().Next(3) < 1;
+
+            (string, int) attack = isSpecialAttack ? attacker.SpecialAttack() : attacker.Attack();
+
+            attacker.LevelUp(defender.TakeDamage(attack, attacker.Name));
         }
     }
 }
